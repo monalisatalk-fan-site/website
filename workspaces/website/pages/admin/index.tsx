56fab238c5028f8bb1e7ab6ae7a0e76386c1fed7
lollipop-onl <lollipop@simochee.net>
2020-10-28 15:33:22 +0900

    ✨ トップページを追加

---
+++ workspaces/website/pages/admin/index.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+import cn from 'classnames';
+import styles from './index.module.scss';
+
+type Props = {
+  className?: string;
+};
+
+const AdminPage: FC<Props> = ({ className }) => {
+  return (
+    <div className={cn(styles.adminPage, className)}>
+      <p>admin top page</p>
+    </div>
+  );
+};
+
+export default AdminPage;
