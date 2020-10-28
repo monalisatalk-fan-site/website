36a8f8edefedf809ffaebd6c8b02a8eb60798a4a
lollipop-onl <lollipop@simochee.net>
2020-10-28 12:23:12 +0900

    ✨ Firebaseのヘルパーを追加

--- workspaces/websites/next.config.js
+++ workspaces/websites/next.config.js
@@ -1,10 +1,33 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 
+require('dotenv').config();
+
+const {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DATABASE_URL,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} = process.env;
+
 /**
  * @type {import('next').NextConfig}
  */
 module.exports = {
+  env: {
+    FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL,
+    FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
