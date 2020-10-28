b8b3180e4b2a6382c8c1758e019c74ef89a6d1ba
lollipop-onl <lollipop@simochee.net>
2020-10-28 09:18:49 +0900

    ✨ トップページファイルを追加

---
+++ workspaces/websites/pages/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import styles from './index.module.scss';
+
+const Index: FC = () => {
+  return <div className={styles.pageContainer}>Hello world.</div>;
+};
+
+export default Index;
