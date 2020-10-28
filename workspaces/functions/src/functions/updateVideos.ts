31d547f670a9ecb414e676248afc178e54b4ba10
lollipop-onl <lollipop@simochee.net>
2020-10-28 12:40:48 +0900

    ⚡️ リソースファイルのパスを変更

--- workspaces/functions/src/functions/updateVideos.ts
+++ workspaces/functions/src/functions/updateVideos.ts
@@ -3,17 +3,14 @@ import * as functions from 'firebase-functions';
 import { fetchYouTubeVideos } from '../helpers/fetchYouTubeVideos';
 
 const bucket = admin.storage().bucket();
-const VIDEO_SNIPPET_FILE = 'resources/video-snippet.json';
-const VIDEO_SNIPPET_META_FILE = 'resources/video-snippet-meta.json';
+const VIDEO_SNIPPET_FILE = 'resources/video-snippet/data.json';
+const VIDEO_SNIPPET_META_FILE = 'resources/video-snippet/meta.json';
 
 export const updateVideos = functions.https.onCall(async () => {
   const items = await fetchYouTubeVideos(true);
   const videoSnippetFile = bucket.file(VIDEO_SNIPPET_FILE);
   const videoSnippetMetaFile = bucket.file(VIDEO_SNIPPET_META_FILE);
-  const timestamp = admin.database.ServerValue.TIMESTAMP;
-
-  console.log(timestamp);
-  console.log(timestamp.toString());
+  const timestamp = new Date().toISOString();
 
   await Promise.all([
     videoSnippetMetaFile.save(
