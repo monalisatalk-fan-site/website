babf686887bb8c1d3019ae4899b85a9cf00e427f
lollipop-onl <lollipop@simochee.net>
2020-11-09 11:43:08 +0900

    ⚡️ fallbackを追加

---
+++ workspaces/admin/pages/videos/_videoId.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  p video detail page.
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  name: 'VideoDetailPage',
+  setup() {},
+});
+</script>
