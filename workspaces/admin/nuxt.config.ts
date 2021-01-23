b4cc4471c1388e19f3e79e915507e60571b737d4
lollipop-onl <lollipop@simochee.net>
2021-01-23 23:06:01 +0900

    ✨ Add firebase module

--- workspaces/admin/nuxt.config.ts
+++ workspaces/admin/nuxt.config.ts
@@ -1,8 +1,42 @@
 import { NuxtConfig } from '@nuxt/types';
+import path from 'path';
+
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
+
+const {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DATABASE_URL,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} = process.env;
 
 const config: NuxtConfig = {
   ssr: false,
   buildModules: ['@nuxt/typescript-build', '@nuxtjs/composition-api'],
+  modules: [
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: FIREBASE_API_KEY,
+          authDomain: FIREBASE_AUTH_DOMAIN,
+          databaseURL: FIREBASE_DATABASE_URL,
+          projectId: FIREBASE_PROJECT_ID,
+          storageBucket: FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+          appId: FIREBASE_APP_ID,
+          measurementId: FIREBASE_MEASUREMENT_ID,
+        },
+        services: {
+          auth: true,
+        },
+      }
+    ]
+  ],
   css: ['@/assets/bootstrap/bootstrap.scss', '@/assets/stisla/style.scss', '@/assets/stisla/components.scss'],
   vue: {
     // @ts-expect-error
