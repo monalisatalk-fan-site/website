3cc99671484b197fe15b9188fd98a0c6cdf0fa34
lollipop-onl <lollipop@simochee.net>
2021-01-23 20:13:58 +0900

    💄 Add stisla styles

--- workspaces/admin/nuxt.config.ts
+++ workspaces/admin/nuxt.config.ts
@@ -3,7 +3,7 @@ import { NuxtConfig } from '@nuxt/types';
 const config: NuxtConfig = {
   ssr: false,
   buildModules: ['@nuxt/typescript-build', '@nuxtjs/composition-api'],
-
+  css: ['@/assets/stisla/scss/style.scss'],
 };
 
 export default config;
