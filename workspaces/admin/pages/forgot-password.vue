d6e632e01746cda2e81c2f3ef50040b273ad7998
lollipop-onl <lollipop@simochee.net>
2021-01-23 23:37:30 +0900

    ✨ Add feature of password reset

--- workspaces/admin/pages/forgot-password.vue
+++ workspaces/admin/pages/forgot-password.vue
@@ -12,13 +12,19 @@
             </template>
             <template #body>
               <p class="text-muted">We will send a link to reset your password.</p>
-              <form @submit.prevent class="needs-validation">
+              <div v-show="errorMessage" class="alert alert-danger">
+                {{errorMessage}}
+              </div>
+              <div v-show="isSent" class="alert alert-success">
+                再設定メールを送信しました
+              </div>
+              <form @submit.prevent="sendPasswordResetEmail" class="needs-validation">
                 <div class="form-group">
                   <label for="email">Email</label>
-                  <input type="email" id="email" class="form-control" required />
+                  <input type="email" id="email" class="form-control" v-model="email" required />
                 </div>
                 <div class="form-group">
-                  <button class="btn btn-primary btn-lg btn-block">Login</button>
+                  <button class="btn btn-primary btn-lg btn-block" :disabled="isLoading || isSent">Send password reset email</button>
                 </div>
               </form>
             </template>
@@ -33,13 +39,48 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api';
+import { defineComponent, ref, useContext } from '@nuxtjs/composition-api';
 
 export default defineComponent({
   name: 'ForgotPasswordPage',
   components: {
     AppCard: () => import('@/components/AppCard.vue'),
   },
-  setup() {},
+  setup() {
+    const { app } = useContext();
+    const isLoading = ref(false);
+    const isSent = ref(false);
+    const email = ref('');
+    const errorMessage = ref('');
+
+    const sendPasswordResetEmail = async () => {
+      try {
+        errorMessage.value = '';
+        isLoading.value = true;
+
+        await app.$fire.auth.sendPasswordResetEmail(email.value);
+
+        isSent.value = true;
+      } catch (err: unknown) {
+        isLoading.value = false;
+
+        if (err instanceof Error) {
+          errorMessage.value = err.message;
+
+          return;
+        }
+
+        errorMessage.value = 'Unknown error';
+      }
+    };
+
+    return {
+      isLoading,
+      isSent,
+      email,
+      errorMessage,
+      sendPasswordResetEmail,
+    }
+  },
 });
 </script>
