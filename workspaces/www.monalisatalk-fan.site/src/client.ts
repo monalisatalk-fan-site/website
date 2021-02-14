684563d9b030eeb9ac560bf60d8ea45934311cbd
lollipop-onl <lollipop@simochee.net>
2021-02-14 18:11:56 +0900

    🛁Clean up www workspace

--- workspaces/www.monalisatalk-fan.site/src/client.ts
+++
@@ -1,6 +0,0 @@
-// @ts-expect-error
-import * as sapper from '@sapper/app';
-
-sapper.start({
-  target: document.getElementById('sapper'),
-});
--- workspaces/www.monalisatalk-fan.site/src/routes/index.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import VideoCardList from '../components/VideoCardList.svelte';
-  import { resources } from '../constants';
-</script>
-
-<VideoCardList videos={resources.videos.slice().splice(0, 10)} layout />
-
-<style lang="scss">
-</style>
