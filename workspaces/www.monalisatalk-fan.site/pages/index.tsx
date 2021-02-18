66a3e226fae57e8f7fc9c8604563d70111d711f3
lollipop-onl <lollipop@simochee.net>
2021-02-19 01:35:27 +0900

    📝 Fixed text

--- workspaces/www.monalisatalk-fan.site/pages/index.tsx
+++ workspaces/www.monalisatalk-fan.site/pages/index.tsx
@@ -5,7 +5,7 @@ export const IndexPage: React.VFC = () => {
   return (
     <div className={styles.indexPage}>
       <img className={styles.logo} src="/images/logo.svg" width="173.76" height="29.808" title="MONA LISA TALK - FAN!!" />
-      <p className={styles.message}>Comming soon</p>
+      <p className={styles.message}>Coming soon</p>
     </div>
   );
 };
