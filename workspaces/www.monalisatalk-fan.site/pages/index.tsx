166eaa3a8b9f986b59ee160c9fbaea03c4fd72ee
lollipop-onl <lollipop@simochee.net>
2021-02-22 22:17:02 +0900

    💄 update media queries

--- workspaces/www.monalisatalk-fan.site/pages/index.tsx
+++ workspaces/www.monalisatalk-fan.site/pages/index.tsx
@@ -1,4 +1,5 @@
 import type React from 'react';
+import clsx from 'clsx';
 import { LayoutContainer } from '~/components/LayoutContainer';
 import { TopHeroView } from '~/components/TopHeroView';
 import styles from './index.module.css';
@@ -9,7 +10,7 @@ export const IndexPage: React.VFC = () => {
       <TopHeroView />
       <LayoutContainer>
         <div className={styles.pageContainer}>
-          <h2 className={styles.heading}>
+          <h2 className={clsx(styles.heading, styles.Right)}>
             <img className={styles.image} src="/images/top/headings/latest-videos.png" alt="最新動画のタイトル" />
           </h2>
           <p>lorem ipsum</p>
