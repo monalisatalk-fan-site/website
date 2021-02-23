3544e13b60803e4dc39c3d934d4fa15f8fdddba2
lollipop-onl <lollipop@simochee.net>
2021-02-23 17:24:03 +0900

    ✨ Add pickup playlist

--- workspaces/www.monalisatalk-fan.site/components/TopHeroView/index.tsx
+++ workspaces/www.monalisatalk-fan.site/components/TopHeroView/index.tsx
@@ -1,48 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import clsx from 'clsx';
 import { YouTubeThumbnail } from '~/components/YouTubeThumbnail';
 import styles from './index.module.css';
 
-export const TopHeroView: React.VFC = () => {
-  const [videos, setVideos] = useState<string[][]>([]);
+export type Props = {
+  videos: string[][];
+};
 
-  useEffect(() => {
-    import('~/assets/data/resources.json').then(({ videos }) => {
-      const results: string[][] = [];
-
-      for (let i = 0; i < 3; i += 1) {
-        const result: string[] = [];
-
-        for (let j = 0; j < 10; j += 1) {
-          const video = videos[Math.floor(Math.random() * videos.length)];
-
-          if (!video) {
-            j -= 1;
-
-            continue;
-          }
-
-          const { id } = video;
-
-          if (
-            result.includes(id) ||
-            results.some((videoIdList) => videoIdList.includes(id))
-          ) {
-            j -= 1;
-
-            continue;
-          }
-
-          result.push(id);
-        }
-
-        results.push(result);
-      }
-
-      setVideos(results);
-    });
-  }, [setVideos]);
-
+export const TopHeroView: React.VFC<Props> = ({ videos }) => {
   return (
     <div className={styles.topHeroView}>
       <div className={clsx(styles.band, styles.Reverse, styles.Red)}></div>
--- workspaces/www.monalisatalk-fan.site/pages/index.tsx
+++ workspaces/www.monalisatalk-fan.site/pages/index.tsx
@@ -1,6 +1,7 @@
-import type React from 'react';
+import React, { useMemo } from 'react';
 import { GetStaticProps } from 'next';
-import clsx from 'clsx';
+import Link from 'next/link';
+import qs from 'qs';
 import { LayoutContainer } from '~/components/LayoutContainer';
 import { TopHeroView } from '~/components/TopHeroView';
 import { SimpleVideoList } from '~/components/SimpleVideoList';
@@ -10,25 +11,64 @@ import styles from './index.module.css';
 
 export type StaticProps = {
   latestVideos: Video[];
+  pickupVideos: string[][];
 };
 
 export const getStaticProps: GetStaticProps<StaticProps> = async () => {
   const { videos } = await import('~/assets/data/resources.json');
   const latestVideos = videos.slice().splice(0, 4);
+  const pickupVideos: string[][] = [];
+
+  for (let i = 0; i < 3; i += 1) {
+    const result: string[] = [];
+
+    for (let j = 0; j < 10; j += 1) {
+      const video = videos[Math.floor(Math.random() * videos.length)];
+
+      if (!video) {
+        j -= 1;
+
+        continue;
+      }
+
+      const { id } = video;
+
+      if (
+        result.includes(id) ||
+        pickupVideos.some((videoIdList) => videoIdList.includes(id))
+      ) {
+        j -= 1;
+
+        continue;
+      }
+
+      result.push(id);
+    }
+
+    pickupVideos.push(result);
+  }
 
   return {
     props: {
       latestVideos,
+      pickupVideos,
     },
   };
 };
 
-export const IndexPage: React.VFC<StaticProps> = ({ latestVideos }) => {
+export const IndexPage: React.VFC<StaticProps> = ({ latestVideos, pickupVideos }) => {
+  const pickupPlaylistLink = useMemo(() => `/playlist?${qs.stringify({ v: pickupVideos.flatMap((videos) => videos) }, { arrayFormat: 'repeat' })}`, [pickupVideos]);
+
   return (
     <div className={styles.indexPage}>
-      <TopHeroView />
+      <TopHeroView videos={pickupVideos} />
       <LayoutContainer>
         <div className={styles.pageContainer}>
+          <div className={styles.pickup}>
+            <Link href={pickupPlaylistLink}>
+              <a className={styles.link}>＞ ピックアッププレイリスト</a>
+            </Link>
+          </div>
           <section className={styles.section}>
             <h1 className={styles.heading}>
               <img
