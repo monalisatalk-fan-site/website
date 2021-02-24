3013447d21f586e76f7975010eae3e2cd21b9372
lollipop-onl <lollipop@simochee.net>
2021-02-24 15:52:20 +0900

    💄 update button style

--- workspaces/www.monalisatalk-fan.site/components/GlobalDrawerMenu/index.tsx
+++ workspaces/www.monalisatalk-fan.site/components/GlobalDrawerMenu/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.module.css';
 export const GlobalDrawerMenu: React.VFC = () => {
   return (
     <>
-      <div className={styles.hambargerMenu}>Hamberger menu</div>
+      <div className={styles.hambargerMenu}>M</div>
       <div className={styles.drawerMenu}>Drawer menu</div>
     </>
   );
