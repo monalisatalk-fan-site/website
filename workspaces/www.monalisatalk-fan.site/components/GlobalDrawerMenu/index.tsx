71f5a131a0c97863c193d49dec61701be940da3f
lollipop-onl <lollipop@simochee.net>
2021-02-24 16:24:29 +0900

    ✨ add drawer menu gimmic

--- workspaces/www.monalisatalk-fan.site/components/GlobalDrawerMenu/index.tsx
+++ workspaces/www.monalisatalk-fan.site/components/GlobalDrawerMenu/index.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import clsx from 'clsx';
 import styles from './index.module.css';
 
 export const GlobalDrawerMenu: React.VFC = () => {
+  const [isMenuVisible, setMenuVisibility] = useState(false);
+  const openMenu = useCallback(() => setMenuVisibility(true), [setMenuVisibility]);
+  const closeMenu = useCallback(() => setMenuVisibility(false), [setMenuVisibility]);
+
   return (
     <>
-      <button className={styles.hambargerMenu}>M</button>
-      <div className={styles.drawerMenu}>Drawer menu</div>
+      <button className={styles.hambargerMenu} onClick={openMenu}>M</button>
+      <div className={clsx(styles.drawerMenu, isMenuVisible && styles.Visible)}>
+        Drawer menu
+        <button onClick={closeMenu}>Close menu</button>
+      </div>
     </>
   );
 };
