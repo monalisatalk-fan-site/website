5e96c217322ac647e7d7ecdb7d20061627f580e5
lollipop-onl <lollipop@simochee.net>
2021-02-24 17:24:41 +0900

    ⚡️ shuffle hero videos

--- workspaces/www.monalisatalk-fan.site/components/TopHeroView/index.tsx
+++ workspaces/www.monalisatalk-fan.site/components/TopHeroView/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import clsx from 'clsx';
 import { YouTubeThumbnail } from '~/components/YouTubeThumbnail';
 import styles from './index.module.css';
@@ -8,11 +8,13 @@ export type Props = {
 };
 
 export const TopHeroView: React.VFC<Props> = ({ videos }) => {
+  const shuffledVideos = useMemo(() => videos.map((videoItems) => videoItems.slice().sort(() => Math.random() - 0.5)), [videos]);
+
   return (
     <div className={styles.topHeroView}>
       <div className={clsx(styles.band, styles.Reverse, styles.Red)}></div>
       <div className={styles.topHeroViewThumbnails}>
-        {videos.map((videoIdList, i) => (
+        {shuffledVideos.map((videoIdList, i) => (
           <div key={i} className={styles.row}>
             {videoIdList.map((videoId) => (
               <div key={videoId} className={styles.item}>
