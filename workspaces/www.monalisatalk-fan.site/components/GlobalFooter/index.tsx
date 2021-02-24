bcd33bc10f521abfecd9c346b3d5afc654f53bbf
lollipop-onl <lollipop-onl@users.noreply.github.com>
2021-02-24 06:43:59 +0000

    👕 Fixed auto-fixable ESLint errors by github-actions

--- workspaces/www.monalisatalk-fan.site/components/GlobalFooter/index.tsx
+++ workspaces/www.monalisatalk-fan.site/components/GlobalFooter/index.tsx
@@ -4,7 +4,9 @@ import styles from './index.module.css';
 export const GlobalFooter: React.VFC = () => {
   return (
     <footer className={styles.globalFooter}>
-      <small className={styles.copyright}>&copy; 2021 モナ・リザの戯言 ファンサイト</small>
+      <small className={styles.copyright}>
+        &copy; 2021 モナ・リザの戯言 ファンサイト
+      </small>
       <div className={styles.hambargerarea} />
     </footer>
   );
